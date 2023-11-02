42cbdc6e55e8bec8f46debb87b2c7c21d7ccdae3
EngineersBox <toastcraft.info@gmail.com>
2023-11-02 23:32:04 +1100

    Added keybindings for split panes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,10 +5,15 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+--[[ THEMING ]]
 config.color_scheme = 'Tomorrow Night'
 config.font = wezterm.font('JetBrainsMono NF')
+config.font_size = 11.0
+config.underline_thickness = 3
+config.underline_position = -4
 config.window_background_opacity = 0.8
 config.text_background_opacity = 0.8
+config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = false
 
@@ -23,6 +28,24 @@ config.colors = {
             italic = false,
             strikethrough = false,
         }
+    }
+}
+
+--[[ KEYMAPS ]]
+config.keys = {
+    {
+        key = ";",
+        mods = "SHIFT|CMD",
+        action = wezterm.action.SplitHorizontal({
+            domain = "CurrentPaneDomain"
+        })
+    },
+    {
+        key = "'",
+        mods = "SHIFT|CMD",
+        action = wezterm.action.SplitVertical({
+            domain = "CurrentPaneDomain"
+        })
     }
 }
 
