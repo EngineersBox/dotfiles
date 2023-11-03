0a230a5f1711bea0c03806ddbf01580e612a4e72
EngineersBox <toastcraft.info@gmail.com>
2023-11-03 23:53:18 +1100

    Added pane switch keybinds for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -45,7 +45,29 @@ config.keys = {
         action = wezterm.action.SplitVertical({
             domain = "CurrentPaneDomain"
         })
-    }
+    },
+    -- activate pane selection mode with the default alphabet (labels are "a", "s", "d", "f" and so on)
+    {
+        key = '8',
+        mods = 'CTRL',
+        action = wezterm.action.PaneSelect
+    },
+    -- activate pane selection mode with numeric labels
+    {
+        key = '9',
+        mods = 'CTRL',
+        action = wezterm.action.PaneSelect {
+            alphabet = '1234567890',
+        },
+    },
+    -- show the pane selection mode, but have it swap the active and selected panes
+    {
+        key = '0',
+        mods = 'CTRL',
+        action = wezterm.action.PaneSelect {
+            mode = 'SwapWithActive',
+        },
+    },
 }
 
 -- [[ STATE ]]
