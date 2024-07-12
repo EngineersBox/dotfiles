0d8585bd590db08920178c586b720e8e75cfb726
EngineersBox <toastcraft.info@gmail.com>
2024-07-13 01:23:00 +1000

    Updated gruvbox config and added modes nvim plugin

--- nvim/lua/plugins/modes.lua
+++ nvim/lua/plugins/modes.lua
@@ -1,46 +1,23 @@
 return {
-	-- {
-	--   "ellisonleao/gruvbox.nvim",
-	--   priority = 1000 ,
-	--   config = function()
-	--       vim.o.background = "dark"
-	--       vim.cmd([[colorscheme gruvbox]])
-	--   end,
-	-- },
 	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			-- Optionally configure and load the colorscheme
-			-- directly inside the plugin declaration.
-			vim.g.gruvbox_material_enable_italic = true
-			vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-			vim.g.gruvbox_material_inlay_hints_background = "dimmed"
-			vim.g.gruvbox_material_diagnostic_line_highlight = 1
-			vim.api.nvim_create_autocmd("ColorScheme", {
-				group = vim.api.nvim_create_augroup("custom_highlights_gruvboxmaterial", {}),
-				pattern = "gruvbox-material",
-				callback = function()
-					local config = vim.fn["gruvbox_material#get_configuration"]()
-					local palette = vim.fn["gruvbox_material#get_palette"](
-						config.background,
-						config.foreground,
-						config.colors_override
-					)
-					local set_hl = vim.fn["gruvbox_material#highlight"]
-					set_hl("NormalMoody", palette.blue, palette.none)
-					set_hl("InsertMoody", palette.none, palette.none)
-					set_hl("VisualMoody", palette.purple, palette.none)
-					set_hl("CommandMoody", palette.green, palette.none)
-					set_hl("ReplaceMoody", palette.orange, palette.none)
-					set_hl("SelectMoody", palette.yellow, palette.none)
-					set_hl("TerminalMoody", palette.aqua, palette.none)
-					set_hl("TerminalNormalMoody", palette.aqua, palette.none)
-					set_hl("CursorLineNr", {"#73d3de", "284"}, palette.none)
-				end,
-			})
-			vim.cmd.colorscheme("gruvbox-material")
-		end,
+		"svampkorg/moody.nvim",
+		event = { "ModeChanged", "BufWinEnter", "WinEnter" },
+		dependencies = {
+			"sainnhe/gruvbox-material",
+		},
+		opts = {
+			-- you can set different blend values for your different modes.
+			-- Some colours might look better more dark.
+			blend = {
+				normal = 0.2,
+				insert = 0.2,
+				visual = 0.3,
+				command = 0.2,
+				replace = 0.2,
+				select = 0.3,
+				terminal = 0.2,
+				terminal_n = 0.2,
+			},
+		},
 	},
 }
