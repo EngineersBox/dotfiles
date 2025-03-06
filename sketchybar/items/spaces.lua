897166e9ebfa7da36f7160bc6aecb8f300147282
EngineersBox <toastcraft.info@gmail.com>
2025-03-06 17:39:06 +1100

    Refactored from io:popen to sbar.exec for aerospace workspace calls when creating spaces in sketchybar

--- sketchybar/items/spaces.lua
+++ sketchybar/items/spaces.lua
@@ -19,6 +19,7 @@ local function parse_string_to_table(s)
     table.insert(result, {
         workspace = "...",
         monitor = 1,
+        -- monitor = 3,
     })
     for line in s:gmatch("([^\n]+)") do
         local split_line = split(line, "%s")
@@ -34,60 +35,59 @@ local function parse_string_to_table(s)
     end
     return result
 end
-
-local file = io.popen("aerospace list-workspaces --all --format '%{workspace} %{monitor-id}'")
-local workspace_monitor_mappings = file:read("*a")
-file:close()
-
-file = io.popen("aerospace list-workspaces --focused --format '%{workspace}'")
-local focused = file:read("*a"):gsub("\n", "")
-file:close()
-
 sbar.add("event", "aerospace_workspace_change")
-local workspaces = parse_string_to_table(workspace_monitor_mappings)
-for i, layout in ipairs(workspaces) do
-    local workspace = layout["workspace"]
-    local monitor = layout["monitor"]
-    local initial_selected = workspace == focused
-	local space = sbar.add("item", workspace, {
-		position = "left",
-        display = monitor,
-        icon = {
-            drawing = false,
-        },
-        label = {
-            padding_left = 5,
-            padding_right = 5,
-            string = workspace,
-            font = {
-                family = "Monocraft",
-                style = settings.font.style_map["Regular"],
-                size = 12,
-            },
-            color = initial_selected and colors.pink or colors.grey,
-        },
-	})
-	table.insert(space_names, workspace)
 
-    space:subscribe("aerospace_workspace_change", function(env)
-        local selected = env.FOCUSED_WORKSPACE == workspace
-        space:set({
-            label = {
-                color = selected and colors.pink or colors.grey, 
-                font = {
-                    style = selected and settings.font.style_map["Bold"] or settings.font.style_map["Regular"],
+sbar.exec("aerospace list-workspaces --all --format '%{workspace} %{monitor-id}'", function (workspace_monitor_mappings)
+    local workspaces = parse_string_to_table(workspace_monitor_mappings)
+    sbar.exec("aerospace list-workspaces --focused --format '%{workspace}'", function(all_workspaces)
+        local focused = all_workspaces:gsub("\n", "")
+        for i, layout in ipairs(workspaces) do
+            local workspace = layout["workspace"]
+            local monitor = layout["monitor"]
+            local initial_selected = workspace == focused
+            local space = sbar.add("item", workspace, {
+                position = "left",
+                display = monitor,
+                icon = {
+                    drawing = false,
                 },
+                label = {
+                    padding_left = 5,
+                    padding_right = 5,
+                    string = workspace,
+                    font = {
+                        family = "Monocraft",
+                        style = settings.font.style_map["Regular"],
+                        size = 12,
+                    },
+                    color = initial_selected and colors.pink or colors.grey,
+                },
+            })
+            table.insert(space_names, workspace)
+
+            space:subscribe("aerospace_workspace_change", function(env)
+                local selected = env.FOCUSED_WORKSPACE == workspace
+                space:set({
+                    label = {
+                        color = selected and colors.pink or colors.grey, 
+                        font = {
+                            style = selected and settings.font.style_map["Bold"] or settings.font.style_map["Regular"],
+                        },
+                    },
+                    associated_display = monitor,
+                })
+            end)
+        end
+
+        sbar.add("bracket", space_names, {
+            background = {
+                border_width = 0,
+                border_color = colors.bar.border,
+                color = colors.bar.bg,
+                width = "dynamic"
             },
-            associated_display = monitor,
         })
     end)
-end
+end)
 
-sbar.add("bracket", space_names, {
-	background = {
-		border_width = 0,
-		border_color = colors.bar.border,
-		color = colors.bar.bg,
-		width = "dynamic"
-	},
-})
+
