951c2032abe8513291300f04ba39c102ed62448a
EngineersBox <engineersbox@noop.com>
2025-05-03 15:01:15 +1000

    Ensured all TS plugins installed in nvim config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -26,13 +26,10 @@ return {
   -- 	},
   -- },
   --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = "all"
+  	},
+  },
 }
