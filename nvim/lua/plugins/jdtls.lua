8b3dd861cbcca88a760b2d534e5147cad445e44d
Jack Kilrain <jack.kilrain@anu.edu.au>
2025-06-16 14:43:16 +1000

    Fixed overlapping diagnostics and added jdtls configs
    Signed-off-by: Jack Kilrain <jack.kilrain@anu.edu.au>

---
+++ nvim/lua/plugins/jdtls.lua
@@ -0,0 +1,66 @@
+return {
+    {
+        "mfussenegger/nvim-jdtls",
+        config = function()
+            local config = {
+                cmd = {
+                    "java",
+                    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+                    "-Dosgi.bundles.defaultStartLevel=4",
+                    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+                    "-Dlog.protocol=true",
+                    "-Dlog.level=ALL",
+                    "-Xmx1g",
+                    "--add-modules=ALL-SYSTEM",
+                    "--add-opens", "java.base/java.util=ALL-UNNAMED",
+                    "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+                    "-jar", "/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher_1.7.0.v20250424-1814.jar",
+                    "-configuration", "/usr/share/java/jdtls/config_linux",
+                    "-data", vim.fn.getcwd(),
+                },
+                root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew", "pom.xml"}),
+                settings = {
+                    java = {
+                        import = {
+                            gradle = {
+                                enabled = true,
+                            },
+                            maven = {
+                                enabled = true,
+                            },
+                            exclusions = {
+                                "**/node_modules/**",
+                                "**/.metadata/**",
+                                "**/archetype-resources/**",
+                                "**/META-INF/maven/**",
+                                "/**/test/**"
+                            }
+                        },
+                        maven = {
+                            downloadSources = true,
+                        },
+                        quickfix = {
+                            enabled = true,
+                        },
+                        rename = {
+                            enabled = true,
+                        },
+                        signatureHelp = {
+                            enabled = true,
+                            description = true,
+                        },
+                        completion = {
+                            importOrder = {
+                                "java",
+                                "javax",
+                                "com",
+                                "org",
+                            },
+                        },
+                    },
+                },
+            }
+            require("jdtls").start_or_attach(config)
+        end,
+    }
+}
--- nvim/lua/plugins/tiny_inline_diagnostic.lua
+++ nvim/lua/plugins/tiny_inline_diagnostic.lua
@@ -4,8 +4,16 @@ return {
         event = "VeryLazy", -- Or `LspAttach`
         priority = 1000, -- needs to be loaded in first
         config = function()
-            require('tiny-inline-diagnostic').setup()
-            vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+            require('tiny-inline-diagnostic').setup({
+                options = {
+                    multilines = {
+                        enabled = true,
+                        always_show = true,
+                    },
+                    show_all_diags_on_cursorline = true,
+                },
+            })
+            -- vim.diagnostic.config({ virtual_text =  }) -- Only if needed in your configuration, if you already have native LSP diagnostics
         end
     }
 }
