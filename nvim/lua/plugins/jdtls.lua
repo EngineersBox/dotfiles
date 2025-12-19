6d011a5cec9f77d5b3d3fe9cecafce1ce0bca20e
EngineersBox <35655145+EngineersBox@users.noreply.github.com>
2025-12-19 23:24:01 +1100

    Generalised jdtls CLI invocation and added charcoal monochrome themes
    Signed-off-by: EngineersBox <35655145+EngineersBox@users.noreply.github.com>

--- nvim/lua/plugins/jdtls.lua
+++ nvim/lua/plugins/jdtls.lua
@@ -3,21 +3,7 @@ return {
         "mfussenegger/nvim-jdtls",
         config = function()
             local config = {
-                cmd = {
-                    "java",
-                    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-                    "-Dosgi.bundles.defaultStartLevel=4",
-                    "-Declipse.product=org.eclipse.jdt.ls.core.product",
-                    "-Dlog.protocol=true",
-                    "-Dlog.level=ALL",
-                    "-Xmx1g",
-                    "--add-modules=ALL-SYSTEM",
-                    "--add-opens", "java.base/java.util=ALL-UNNAMED",
-                    "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-                    "-jar", "/opt/homebrew/opt/jdtls/libexec/plugins/org.eclipse.equinox.launcher.cocoa.macosx.aarch64_1.2.1300.v20250331-1702.jar",
-                    "-configuration", "/opt/homebrew/opt/jdtls/libexec/config_mac",
-                    "-data", vim.fn.getcwd(),
-                },
+                cmd = {"jdtls", "-data", vim.fn.getcwd()},
                 root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew", "pom.xml"}),
                 settings = {
                     java = {
